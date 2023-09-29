34d6add8259a9d09eaf1636bd22d381e6c8ddfa6
Ivanov-s-tmn <ivanov.sappleid@gmail.com>
2023-09-29 17:34:57 +0500

    minor changes

--- ActionCounter/src/ActionCounter.java
+++ ActionCounter/src/ActionCounter.java
@@ -23,7 +23,7 @@ public class ActionCounter {
 
     public int getActions(int timestamp) {
         return (int) hits.stream()
-                .filter(hit -> (hit > timestamp - TIMESTAMP_INTERVAL_SEC) && (hit <= timestamp)) //фильтруем по времени обращения (за последние 5 минут), т.е. при указании timestamp(301), посчитаются обращения за время от 2 до 301 секунд включительно
+                .filter(hit -> hit > timestamp - TIMESTAMP_INTERVAL_SEC) //фильтруем по времени обращения (за последние 5 минут), т.е. при указании timestamp(301), посчитаются обращения за время от 2 до 301 секунд включительно
                 .count();
     }
 }
@@ -36,16 +36,16 @@ class Main {
         actionCounter.call(2);
         actionCounter.call(3);
         actionCounter.call(4);
+        System.out.println(actionCounter.getActions(4)); //возвращает 5 обращений
         actionCounter.call(5);
         actionCounter.call(6);
         actionCounter.call(7);
-        System.out.println(actionCounter.getActions(4)); //возвращает 3 обращения
         actionCounter.call(300);
-        System.out.println(actionCounter.getActions(300)); //возвращает 4 обращения
-        System.out.println(actionCounter.getActions(301)); //возвращает 3 обращения
+        System.out.println(actionCounter.getActions(300)); //возвращает 9 обращений
+        System.out.println(actionCounter.getActions(301)); //возвращает 8 обращений
+        System.out.println(actionCounter.getActions(19500)); //возвращает 0 обращений
         actionCounter.call(19523);
-        actionCounter.call(19801);
-        System.out.println(actionCounter.getActions(19500));
-        System.out.println(actionCounter.getActions(19800));
+        actionCounter.call(19799);
+        System.out.println(actionCounter.getActions(19800)); //возвращает 2 обращения
     }
 }
